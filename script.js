e2f3a9524a05d0adf3785b2ec7c4d8e59908c959
barbozaa123 <139371997+barbozaa123@users.noreply.github.com>
2023-11-19 17:43:34 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+/*Coincidencias Basicas
+        .       - Cualquier Caracter, excepto nueva linea
+        \       - Indica que el siguiente caracter se debe tratar de manera especial o "escaparse".
+        \d      - Cualquier Digitos (0-9)
+        \D      - No es un Digito (0-9)
+        \w      - Caracter de Palabra (a-z, A-Z, 0-9, _)
+        \W      - No es un Caracter de Palabra.
+        \s      - Espacios de cualquier tipo. (espacio, tab, nueva linea)
+        \S      - No es un Espacio, Tab o nueva linea.
+
+        Limites
+        \b      - Limite de Palabra
+        \B      - No es un Limite de Palabra
+        ^       - Inicio de una cadena de texto
+        $       - Final de una cadena de texto
+
+        Cuantificadores:
+        *       - Coincide con 0 o más ocurrencias del patrón anterior.
+        +       - Coincide con 1 o más ocurrencias del patrón anterior.
+        ?       - Coincide con 0 o 1 ocurrencia del patrón anterior.
+        {3}     - Numero Exacto
+        {3,4}   - Rango de Numeros (Minimo, Maximo)
+
+        Conjuntos de Caracteres
+        []      - Caracteres dentro de los brackets
+        [^ ]    - Caracteres que NO ESTAN dentro de los brackets
+
+        Grupos
+        ( )     - Grupo
+        |       - Uno u otro 
+        */
+const regexPass = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{7,}$/;
+
+function validarPass(password) {
+  return regexPass.test(password);
+}
+
+// Validar si la contraseña cumple las condiciones
+password = prompt("Introduce la contraseña!")
+if (validarPass(password)) {
+  alert("Felicidades! Tu contraseña es válida.");
+} else {
+  alert("Intenta de nuevo, recuerda que sólo tienes un intento (si fallas debes recargar la página).");
+  password = prompt("Introduce la contraseña!");
+}
+
+
